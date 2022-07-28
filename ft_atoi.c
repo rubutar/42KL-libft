efe8c1943723442f692b9eac52430d6d430bc408
RUDI JUANDI BUTARBUTAR <830100520@intranet.pajak.go.id>
2022-07-28 17:40:58 +0700

    0.21

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,23 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
-// int atoi(const char *str)
+int   ft_atoi(const char *str)
+{
+   int   i;
+   int   is_neg;
+   int   res;
 
-int main () {
-   int val;
-   char str[20];
+   if (!str)
+      return (0);
+   i = 0
+   while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+      i++;
    
-   strcpy(str, "--++98993489");
-   val = atoi(str);
-   printf("String value = %s, Int value = %d\n", str, val);
-
-   strcpy(str, "tutorialspoint.com");
-   val = atoi(str);
-   printf("String value = %s, Int value = %d\n", str, val);
-
-   return(0);
+   is_neg = (str[i] == '-') ? -1 : 1;
+   if (is_neg == -1 || str[i] == '+')
+      i++;
+   res = 0;
+   while (str[i] >= '0' && str[i] <= '9')
+      res = (res * 10) + (str[i++] - '0');
+   return (res * is_neg);
 }
